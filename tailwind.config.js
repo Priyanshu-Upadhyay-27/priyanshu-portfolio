4c9db2832e9852e5099c7cfda2c5ba99e839d33d
Priyanshu-Upadhyay-27 <160316707+Priyanshu-Upadhyay-27@users.noreply.github.com>
2026-03-31 22:22:50 +0530

    Updated tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'charcoal': '#121212',
+        'near-black': '#0a0a0a',
+        'soft-white': '#f5f5f5',
+        'teal': '#008080',
+        'muted-green': '#5A7D7C',
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        display: ['Outfit', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
